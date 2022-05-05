b04867ccbc8f7eb74d6f07d97ad57efc8eb04cfc
Yifan <372104922@qq.com>
2022-05-05 20:57:11 +0800

    Add project files.

---
+++ MyTravel.API/Controllers/ShoudongAPI.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MyTravel.API.Controllers
+{
+    [Route("api/shoudongapi")]
+    //[Controller]
+    //public class ShoudongAPIController
+    public class ShoudongAPI : Controller
+
+    {
+        // GET: api/<TestAPIController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
