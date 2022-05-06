a1e9d4e09b7e8e0cc91a85c108300e73ed3a5d62
Yifan <372104922@qq.com>
2022-05-06 15:42:51 +0800

    feat: add initial data

--- MyTravel.API/Database/AppDbContext.cs
+++ MyTravel.API/Database/AppDbContext.cs
@@ -1,16 +1,44 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
 using MyTravel.API.Models;
 
 namespace MyTravel.API.Database
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-
         }
 
         public DbSet<TouristRoute> TouristRoutes { get; set; }
         public DbSet<TouristRoutePicture> TouristRoutePictures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<TouristRoute>().HasData(new TouristRoute()
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Title = "ceshititle",
+            //    Description = "shuoming",
+            //    OriginalPrice = 0,
+            //    CreateTime = DateTime.UtcNow
+            //});
+
+            var touristRouteJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutesMockData.json");
+            IList<TouristRoute> touristRoutes = JsonConvert.DeserializeObject<IList<TouristRoute>>(touristRouteJsonData);
+            modelBuilder.Entity<TouristRoute>().HasData(touristRoutes);
+
+            var touristRoutePictureJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutePicturesMockData.json");
+            IList<TouristRoutePicture> touristRoutePictures = JsonConvert.DeserializeObject<IList<TouristRoutePicture>>(touristRoutePictureJsonData);
+            modelBuilder.Entity<TouristRoutePicture>().HasData(touristRoutePictures);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
